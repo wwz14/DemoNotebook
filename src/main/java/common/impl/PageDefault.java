45cb1ba7f83fac3df7a158f29ef44662947e5647
iDimple <wqy14@software.nju.edu.cn>
2016-12-28 15:52:45 +0800

    implements serializable

--- src/main/java/common/impl/PageDefault.java
+++ src/main/java/common/impl/PageDefault.java
@@ -3,12 +3,13 @@ package common.impl;
 import common.Page;
 import common.Position;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by 徐江河 on 2016/12/24.
  */
-public class PageDefault implements Page {
+public class PageDefault implements Page ,Serializable {
 
     private ArrayList<Position> view;
     private int number;
